a1c328be2a445c3704d4900310c386a77b7b260e
Devin Ercolano <devin.ercolano@gmail.com>
2021-02-01 09:41:09 -0800

    Updated some css in route group templates. Tired to revive Token Sale App, not working in Wallet page. Fixed the navbar to include /auth/wallet. /new-payment still not working.

--- assets/js/wallet.js
+++ assets/js/wallet.js
@@ -1,11 +1,12 @@
 const Portis = require('@portis/web3');
+const Web3 = require('web3');
 const portis = new Portis('5946cfd4-f46d-4b09-b579-020a69690dee', 'rinkeby');
 const web3 = new Web3(portis.provider);
 
 
 $(window).on('load', function() {
     // Leads to the ability to buy and recieve Ether/other currencies
-    document.getElementById("showPortis").onclick = () => portis.showPortis();
+    document.getElementById("showPortis").onclick = () => portis.provider.enable();
     document.getElementById("Logout").onclick = () => portis.logout();
 
     portis.onLogin((walletAddress) => {
@@ -23,4 +24,8 @@ $(window).on('load', function() {
       });
 
       portis.showPortis();
+
+      portis.onError(error => {
+        console.log('error', error);
+      });
 });
